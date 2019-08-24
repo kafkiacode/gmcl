14a2916cd14277549bc4df043f33222a4d847b4c
GM <gabrielmerida@gmail.com>
2019-08-24 15:47:27 +1000

    Updates

---
+++ demos/BlackTarCity/0.1/precache-manifest.aec92d0053b28ad484a7a4b3750ef07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a478c34b4e3c02249bd53ccd9aba752",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f375cd7800a09a1c144f",
+    "url": "/static/css/main.22652383.chunk.css"
+  },
+  {
+    "revision": "2b3f03a6b3927ff453fd",
+    "url": "/static/js/2.ebce4589.chunk.js"
+  },
+  {
+    "revision": "f375cd7800a09a1c144f",
+    "url": "/static/js/main.d71cf9f0.chunk.js"
+  },
+  {
+    "revision": "4ddc629bb3930d6e7d07",
+    "url": "/static/js/runtime~main.4cabafa8.js"
+  }
+]);
